10c4f37dc1c45fea831e2b4489010cd17d4433d4
muyaho-universe <kimdaesk@gmail.com>
2022-06-21 15:13:33 +0900

    Complete number buttons panel

--- app/src/main/java/com/dale/calculator/Buttons.java
+++ app/src/main/java/com/dale/calculator/Buttons.java
@@ -6,11 +6,10 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Buttons extends JButton{
-	private int buttonWidht = 100;
-	private int buttonheight = 100;
+	static int buttonWidht = 100;
+	static int buttonheight = 100;
 
-	public Buttons(String elements, int x, int y) {
+	public Buttons(String elements) {
 		super(elements);
-		this.setSize(buttonWidht,buttonheight);
 	}
 }
--- app/src/main/java/com/dale/calculator/CalculatorGUI.java
+++ app/src/main/java/com/dale/calculator/CalculatorGUI.java
@@ -7,22 +7,54 @@ import javax.swing.JFrame;
 public class CalculatorGUI extends JFrame{
 	private static final long serialVersionUID = 1L;
 	private int mainWidth = 400;
-	private int mainHeight = 650;
+	private int mainHeight = 680;
 	final private String TITLE = "Caculator";
 	
 	JFrame jFrame = new JFrame(TITLE);
-	Buttons [] btn = new Buttons [16]; //16개 생성
+	Buttons [] btn = new Buttons [20];
 	
 	public void createFrame() {
-		int x = 0, y =0;
+		int x = 200, y =0;
 		jFrame.setLayout(null);
 		Container mainContainer = jFrame.getContentPane();
 		Panel numberButtonsPanel =  new Panel();
 		Panel resultPrintPanel =  new Panel();
 		Panel additionalButtonsPanel = new Panel();
 		Panel arithmeticOperationsButtonsPanel =  new Panel();
+				
+		numberButtonsPanel.setLayout(null);
+		resultPrintPanel.setLayout(null);
+		additionalButtonsPanel.setLayout(null);
+		arithmeticOperationsButtonsPanel.setLayout(null);
 		
+		btn[0] = new Buttons(0+"");
+		btn[0].setBounds(0, 300, Buttons.buttonWidht*2, Buttons.buttonheight);
+		numberButtonsPanel.add(btn[0]);
+		for(int i = 9; i >= 1; i--) {
+			btn[i] = new Buttons(i+"");
+			
+			numberButtonsPanel.add(btn[i]);
+			btn[i].setBounds(x, y, Buttons.buttonWidht, Buttons.buttonheight);
+			
+			if((i-1) %3 ==0) {
+				x = 200;
+				y += 100;
+			}
+			else {
+				x -= 100;
+			}
+		}		
+		btn[10] = new Buttons(".");
+		btn[10].setBounds(200, 300, Buttons.buttonWidht, Buttons.buttonheight);
+		numberButtonsPanel.add(btn[10]);
 		
+		btn[11] = new Buttons("+");
+		btn[11].setBounds(0,0, Buttons.buttonWidht, Buttons.buttonheight);
+		arithmeticOperationsButtonsPanel.add(btn[11]);
+//		btn[12] = new Buttons("-");
+//		btn[13] = new Buttons("*");
+//		btn[14] = new Buttons("/");
+//		btn[15] = new Buttons("=");
 		
 		
 		numberButtonsPanel.setBounds( 0, 250, mainWidth/4*3, 400);
@@ -37,31 +69,11 @@ public class CalculatorGUI extends JFrame{
 		arithmeticOperationsButtonsPanel.setBounds( 300, 150, mainWidth/4, mainHeight -150);
 		arithmeticOperationsButtonsPanel.setBackground(Color.blue);
 		
-		
 		mainContainer.setLayout(null);
 		mainContainer.add(numberButtonsPanel);
 		mainContainer.add(resultPrintPanel);
 		mainContainer.add(additionalButtonsPanel);
 		mainContainer.add(arithmeticOperationsButtonsPanel);
-		
-		for(int i = 0; i < 10; i++) {
-			
-			btn[i] = new Buttons(i+"", x, y);
-			numberButtonsPanel.add(btn[i]);
-			if((i+1) %3 ==0) {
-				x = 0;
-				y += 100;
-			}
-			x += 100;
-		}
-		
-//		btn[11] = new Buttons("+");
-//		btn[11].setBounds(0,40,width,height);
-//		btn[12] = new Buttons("-");
-//		btn[13] = new Buttons("*");
-//		btn[14] = new Buttons("/");
-//		btn[15] = new Buttons("=");
-		
 		
 		//프레임 크기 지정	
 		jFrame.setSize(mainWidth, mainHeight);
