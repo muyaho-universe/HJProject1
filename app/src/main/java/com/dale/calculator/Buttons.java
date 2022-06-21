c1e1c587682d69b91e2051c438d5e273196b5daa
muyaho-universe <kimdaesk@gmail.com>
2022-06-21 13:53:24 +0900

    Working on GUI

--- app/src/main/java/com/dale/calculator/Buttons.java
+++ app/src/main/java/com/dale/calculator/Buttons.java
@@ -5,13 +5,12 @@ import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class Buttons extends CalculatorGUI{
-	
-	private String buttonName = null;
+public class Buttons extends JButton{
+	private int buttonWidht = 50;
+	private int buttonheight = 50;
 
-	public Buttons(String elements) {
-		buttonName = elements;
-		JButton buttonName = new JButton("elements");
-		
+	public Buttons(String elements, int x, int y) {
+		super(elements);
+		this.setBounds(,30,buttonWidht,buttonheight);
 	}
 }
--- app/src/main/java/com/dale/calculator/CalculatorGUI.java
+++ app/src/main/java/com/dale/calculator/CalculatorGUI.java
@@ -1,29 +1,51 @@
 package com.dale.calculator;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import javax.swing.JButton;
+import java.awt.*;
+
 import javax.swing.JFrame;
 
 public class CalculatorGUI extends JFrame{
-	private int width = 100;
-	private int height = 100;
-	static private String title = null;
+	private static final long serialVersionUID = 1L;
+	private int mainWidth = 400;
+	private int mainHeight = 600;
+	final private String TITLE = "Caculator";
 	
-	JFrame frame = new JFrame("Caculator");
-	JButton [] btn = new JButton [6]; //6개 생성
+	JFrame jFrame = new JFrame(TITLE);
+	Buttons [] btn = new Buttons [16]; //16개 생성
 	
 	public void createFrame() {
+		jFrame.setLayout(null);
+		Container mainContainer = jFrame.getContentPane();
+		Container numberButtonsContainer =  jFrame.getContentPane();
+		int x = 0;
+		
+		
+		mainContainer.setLayout(new FlowLayout());
+		mainContainer.add(numberButtonsContainer);
+		numberButtonsContainer.setBounds( 0, 300, mainWidth/2, mainHeight/2);
+		for(int y = 0; y < 10; y++) {
+			
+			btn[y] = new Buttons(y+"", x, y);
+			numberButtonsContainer.add(btn[y]);
+				
+		}
+		
+		btn[11] = new Buttons("+");
+		btn[11].setBounds(0,40,width,height);
+		btn[12] = new Buttons("-");
+		btn[13] = new Buttons("*");
+		btn[14] = new Buttons("/");
+		btn[15] = new Buttons("=");
 		
 		
 		//프레임 크기 지정	
-		frame.setSize(300, 600);
+		jFrame.setSize(300, 600);
 
 		//프레임 보이기
-		frame.setVisible(true);
+		jFrame.setVisible(true);
 
 		//swing에만 있는 X버튼 클릭시 종료
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	
